e34633589e4d14ef63173c148edcdcca031cf41b
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-01-09 00:27:41 +0100

    cosmetics update

--- Projekt/frontend/src/ui/nfts/NftList.js
+++ Projekt/frontend/src/ui/nfts/NftList.js
@@ -1,4 +1,4 @@
-import {connect } from "react-redux"; 
+import { connect } from "react-redux"; 
 import { getNfts } from "../../ducks/nfts/selectors";
 
 const NftList = ({ nfts, getNftList } ,props) => {
--- Projekt/frontend/src/ui/users/UserList.js
+++ Projekt/frontend/src/ui/users/UserList.js
@@ -1,4 +1,4 @@
-import {connect } from "react-redux"; 
+import { connect } from "react-redux"; 
 import { getUsers } from "../../ducks/users/selectors";
 import { deleteUser } from "../../ducks/users/operations"
 
