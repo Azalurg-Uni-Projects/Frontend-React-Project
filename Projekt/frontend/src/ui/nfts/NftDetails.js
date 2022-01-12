57f1a2a27817d1e911ce9865bdfa27a80ab7648b
Azalurg <91213389+Azalurg@users.noreply.github.com>
2022-01-12 22:14:22 +0100

    Nft update

--- Projekt/frontend/src/ui/nfts/NftDetails.js
+++ Projekt/frontend/src/ui/nfts/NftDetails.js
@@ -1,35 +1,34 @@
 import { connect } from "react-redux";
 import { getUsers } from "../../ducks/users/selectors";
 import { getNfts } from "../../ducks/nfts/selectors";
-import { getCollectios } from "../../ducks/collections/selectors";
+import { getCollections } from "../../ducks/collections/selectors";
 import { Link } from "react-router-dom";
 
 const NftDetails =({ users, collections, nfts } ,props) => {
-    let id = window.location.pathname.slice(15)
-    const nft = nfts.filter(nft => nft._id === id)
-    const owner = users.filter(user => user._id === nft.owner_id)
-    const author = users.filter(user => user._id === nft.author_id)
-    const collection = collections.filter(collection => collection.id === nft.collection_id)
-
+    let id = window.location.pathname.slice(14)
+    const nft = nfts.filter(nft => nft._id === id)[0]
+    const owner = users.filter(user => user._id === nft.owner_id)[0]
+    const author = users.filter(user => user._id === nft.author_id)[0]
+    const collection = collections.filter(collection => collection._id === nft.collection_id)[0]
     return(
         <div className="Details">
             <div className="Back">
-                <Link to="/nfts" className="Bnt">Back</Link>
+                <Link to="/nfts" className="Btn">Back</Link>
             </div>
             <div className="Info">
                 <section>
-                    <h1>{nft.title}</h1>
                     <br/>
-                    <p>price: {nft.price}{nft.currency}</p>
+                    <p>price: {nft.price} {nft.currency}</p>
                     <p>created date: {nft.created_date}</p>
-                    <p>collection:<Link to={`collections/details/${collection._id}`}>{collection.name}</Link></p>
-                    <p>autor:<Link to={`users/details/${author._id}`}>{author.name}</Link></p>
-                    <p>owner:<Link to={`users/details/${owner._id}`}>{owner.nickname}</Link></p>
+                    collection: <Link className="Link" to={`/collections/details/${collection._id}`}>{collection.name}</Link><br/><br/>
+                    autor: <Link className="Link" to={`/users/details/${author._id}`}>{author.nickname}</Link><br/><br/>
+                    owner: <Link className="Link" to={`/users/details/${owner._id}`}>{owner.nickname}</Link><br/><br/>
                 </section>
             </div>
             <div className="Colleted">  
                 <section>
-                <img src={user.logo_url} alt = "NFT NOT FOUND"/>
+                <h1>{nft.title}</h1>
+                <img src={nft.image_url} alt = "NFT NOT FOUND"/>
                 <p>{nft.description}</p>
                 </section>
             </div>
@@ -41,7 +40,7 @@ const mapStateToProps = (state) => {
     return {
         users: getUsers(state),
         nfts: getNfts(state),
-        collections: getCollectios(state)
+        collections: getCollections(state)
     };
 }
 const mapDispatchToProps = {
--- Projekt/frontend/src/ui/nfts/NftList.js
+++ Projekt/frontend/src/ui/nfts/NftList.js
@@ -1,18 +1,19 @@
 import { connect } from "react-redux"; 
 import { getNfts } from "../../ducks/nfts/selectors";
 import { deleteNft } from "../../ducks/nfts/operations";
+import { Link } from "react-router-dom";
 
 const NftList = ({ nfts, deleteNft} ,props) => {
-    function shuffleArray(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            let temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-        return array
-    }
-    nfts = shuffleArray(nfts)
+    // function shuffleArray(array) {
+    //     for (let i = array.length - 1; i > 0; i--) {
+    //         let j = Math.floor(Math.random() * (i + 1));
+    //         let temp = array[i];
+    //         array[i] = array[j];
+    //         array[j] = temp;
+    //     }
+    //     return array
+    // }
+    // nfts = shuffleArray(nfts)
 
     return(
         <div>
@@ -20,9 +21,11 @@ const NftList = ({ nfts, deleteNft} ,props) => {
             <div className="List">
                 {nfts ? nfts.map(nft =>(
                     <div key={nft._id} className="ListContainer">
-                    <img className="Small-img" src={nft.image_url} alt = "NFT NOT FOUND"/>
-                    <p>{nft.title}</p>
-                    <button className="Btn Delete" onClick={() => {deleteNft(nft)}}>Delete</button>
+                        <Link to={`/nfts/details/${nft._id}`}>
+                            <img className="Small-img" src={nft.image_url} alt = "NFT NOT FOUND"/>
+                        </Link>
+                        <p>{nft.title}</p>
+                        <button className="Btn Delete" onClick={() => {deleteNft(nft)}}>Delete</button>
                     </div>
                 )) : <div>No one here</div>}
             </div>
